85d1a5a90940e23b9d9939711e90c67db22ab900
Stavros Thalassinos <stavros8003@gmail.com>
2024-02-19 17:02:52 +0200

    changed to modelform

--- reviews/forms.py
+++ reviews/forms.py
@@ -1,7 +1,15 @@
 from django import forms
 
-class ReviewForm(forms.Form):
-    user_name = forms.CharField(label='Your Name', max_length=100, 
-                                error_messages={'required': 'Please enter your name', 'max_length': 'Please enter a shorter name'})
-    review_text = forms.CharField(label='Your Feedback', widget=forms.Textarea, max_length=200, error_messages={'required': 'Please enter your feedback'})
-    rating = forms.IntegerField(label='Your Rating', min_value=1, max_value=5, error_messages={'required': 'Please enter a rating', 'min_value': 'Please enter a value between 1 and 5', 'max_value': 'Please enter a value between 1 and 5'})
+from .models import Review
+
+# class ReviewForm(forms.Form):
+#     user_name = forms.CharField(label='Your Name', max_length=100, 
+#                                 error_messages={'required': 'Please enter your name', 'max_length': 'Please enter a shorter name'})
+#     review_text = forms.CharField(label='Your Feedback', widget=forms.Textarea, max_length=200, error_messages={'required': 'Please enter your feedback'})
+#     rating = forms.IntegerField(label='Your Rating', min_value=1, max_value=5, error_messages={'required': 'Please enter a rating', 'min_value': 'Please enter a value between 1 and 5', 'max_value': 'Please enter a value between 1 and 5'})
+
+
+class ReviewForm(forms.ModelForm):
+   class Meta: 
+       model = Review
+       fields = '__all__'
