ca43a7a283073a29d6ff1cbd23ff7f2a1225f0d9
Stavros Thalassinos <stavros8003@gmail.com>
2024-02-20 22:49:01 +0200

    added new list view

--- profiles/urls.py
+++ profiles/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.CreateProfileView.as_view(), name="create_profile"),  # Corrected
+    path("", views.CreateProfileView.as_view(), name="create_profile"),
+    path("/all", views.ProfileListView.as_view(), name="profile_list")
 ]
--- profiles/views.py
+++ profiles/views.py
@@ -2,7 +2,8 @@ from django.shortcuts import render
 from django.urls import reverse_lazy
 from django.http import HttpResponseRedirect
 from django.views import View
-from django.views.generic.edit import FormView
+from django.views.generic.edit import FormView, CreateView
+from django.views.generic import ListView
 
 from .forms import ProfileForm
 from .models import Profile
@@ -12,23 +13,35 @@ def store_file(file):
     with open("temp/profile_pic.jpg", "wb+") as destination:
         for chunk in file.chunks():
             destination.write(chunk)
+            
+class CreateProfileView(CreateView):
+    template_name = "profiles/create_profile.html"
+    model = Profile
+    fields = "__all__"
+    success_url = "/profiles"
+    
 
-class CreateProfileView(View):
-    def get(self, request):
-        form = ProfileForm()
-        return render(request, "profiles/create_profile.html", {"form": form})
+class ProfileListView(ListView):
+    template_name = "profiles/user_profiles.html"
+    model = Profile
+    context_object_name = "profiles"
 
-    def post(self,request): 
-        store_file(request.FILES["profile_pic"]) # stores the file in the temp folder
-        submitted_form = ProfileForm(request.POST, request.FILES)
+# class CreateProfileView(View):
+#     def get(self, request):
+#         form = ProfileForm()
+#         return render(request, "profiles/create_profile.html", {"form": form})
+
+#     def post(self,request): 
+#         store_file(request.FILES["profile_pic"]) # stores the file in the temp folder
+#         submitted_form = ProfileForm(request.POST, request.FILES)
         
-        if submitted_form.is_valid():
-            profile = Profile(profile_pic=request.FILES["profile_pic"],
-                              first_name=submitted_form.cleaned_data["first_name"],
-                              last_name=submitted_form.cleaned_data["last_name"],
-                              bio=submitted_form.cleaned_data["bio"],
-                              email=submitted_form.cleaned_data["email"])
-            profile.save()
-            return HttpResponseRedirect(reverse_lazy("create_profile"))
+#         if submitted_form.is_valid():
+#             profile = Profile(profile_pic=request.FILES["profile_pic"],
+#                               first_name=submitted_form.cleaned_data["first_name"],
+#                               last_name=submitted_form.cleaned_data["last_name"],
+#                               bio=submitted_form.cleaned_data["bio"],
+#                               email=submitted_form.cleaned_data["email"])
+#             profile.save()
+#             return HttpResponseRedirect(reverse_lazy("create_profile"))
         
-        return HttpResponseRedirect(reverse_lazy("create_profile"))
+#         return HttpResponseRedirect(reverse_lazy("create_profile"))
